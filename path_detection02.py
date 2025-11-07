4d984d40b49ed0a718d39fb02e4344d8e40f8f8b
Kefeat Ullah <kefaet.cp@gmail.com>
2025-11-07 12:53:41 +0600

    second one

--- path_detection02.py
+++ path_detection02.py
@@ -2,18 +2,23 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 from deep_sort_realtime.deepsort_tracker import DeepSort
+import torchreid
 
 # Load YOLOv8 model
 model = YOLO("yolov8n.pt")
 
 # Initialize DeepSORT tracker
-tracker = DeepSort(max_age=30,embedder="mobilenet")
+tracker = DeepSort(max_age=90,embedder="torchreid",
+    embedder_model_name="resnet50",   # or "resnet50"
+    half=True  )
 
 # Open video file
 cap = cv2.VideoCapture("C:\\Users\\LENOVO\\Videos\\Screen Recordings\\Screen Recording 2025-11-04 064232.mp4")
 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 black_bg = np.zeros((h, w, 3), dtype=np.uint8)
+
+id_colors = {}
 
 while True:
     ret, frame = cap.read()
@@ -42,15 +47,19 @@ while True:
             continue
         x1, y1, x2, y2 = map(int, t.to_ltrb())
 
-        track_id = t.track_id
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.circle(black_bg, ((x1 + x2) // 2, (y1 + y2) // 2), 2, (
-    (int(track_id) * 37) % 255,
-    (int(track_id) * 17) % 255,
-    (int(track_id) * 29) % 255
-), -1)
+        track_id = int(t.track_id)
+
+        # assign a color if this ID is new
+        if track_id not in id_colors:
+            id_colors[track_id] = ((track_id * 37) % 255,(track_id * 17) % 255,(track_id * 29) % 255)
+        
+        color = id_colors[track_id]
+
+        # draw on frames
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+        cv2.circle(black_bg, ((x1 + x2) // 2, (y1 + y2) // 2), 2, color, -1)
         cv2.putText(frame, f'ID {track_id}', (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
 
     cv2.imshow("People Detection and Tracking", frame)
 
@@ -58,8 +67,16 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cv2.imwrite("motion_paths.png", black_bg)
+
+cv2.imwrite("motion_paths02_torchid_osnet.png", black_bg)
 cv2.imshow("Motion Paths", black_bg)
 cv2.waitKey(0)
 cap.release()
 cv2.destroyAllWindows()
+
+print(f"Total unique IDs: {len(id_colors)}")
+print(id_colors)
+import json
+with open("id_color_map.json", "w") as f:
+    json.dump(id_colors, f)
+
